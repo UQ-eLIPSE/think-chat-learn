f417f95f0ab1f1fe746e779efcf4aef560422c38
James Li <james.li1@uq.edu.au>
2016-07-14 12:12:16 +1000

    Changed help message; Improved receipt info

--- src/client/js/moocchat2/main.ts
+++ src/client/js/moocchat2/main.ts
@@ -197,11 +197,24 @@ $(() => {
 
                     // Session ID is split every 4th character to make it easier to read
                     page$("#session-id").text(session.id.match(/.{1,4}/g).join(" "));
-                    page$("#time-now").text(new Date().toISOString());
+                    // page$("#time-now").text(new Date().toISOString());
+
+                    page$("#print-receipt").on("click", () => {
+                        window.print();
+                    });
 
                     page$("#go-to-return-url").on("click", () => {
                         window.top.location.href = _LTI_BASIC_LAUNCH_DATA.launch_presentation_return_url;
                     });
+
+                    let initialAnswer = session.quiz.questionOptions.filter((option) => option._id === session.answers.initial.optionId)[0];
+                    let revisedAnswer = session.quiz.questionOptions.filter((option) => option._id === session.answers.revised.optionId)[0];
+
+                    page$("#initial-answer-content").html(initialAnswer.content);
+                    page$("#initial-answer-justification").text(session.answers.initial.justification);
+
+                    page$("#revised-answer-content").html(revisedAnswer.content);
+                    page$("#revised-answer-justification").text(session.answers.revised.justification);
                 });
             }
         }
