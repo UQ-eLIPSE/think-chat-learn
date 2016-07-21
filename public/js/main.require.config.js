e2e741e6f24a1bc9a3716cf65b1a8ef1b4972715
James Li <james.li1@uq.edu.au>
2016-07-21 14:52:23 +1000

    Bringing external libraries into project

--- public/js/main.require.config.js
+++ public/js/main.require.config.js
@@ -4,8 +4,8 @@ requirejs.config({
 
     // All references to external resources
     paths: {
-        "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min",
-        "socket.io-client": "https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.6/socket.io.min"
+        "jquery": "../lib/jquery/3.0.0/jquery.min",
+        "socket.io-client": "../lib/socket.io/1.4.6/socket.io.min"
     },
 
     // App entry point
