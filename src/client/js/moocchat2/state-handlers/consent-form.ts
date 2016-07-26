3817d20f099d51278dfc960b249ce53c582e9ed7
James Li <james.li1@uq.edu.au>
2016-07-26 11:02:53 +1000

    Consent form revisions to include longer blurb

--- src/client/js/moocchat2/state-handlers/consent-form.ts
+++ src/client/js/moocchat2/state-handlers/consent-form.ts
@@ -4,6 +4,10 @@ import {IStateHandler} from "../classes/IStateHandler";
 
 import {MoocchatState as STATE} from "../classes/MoocchatStates";
 
+import {ILTIBasicLaunchData} from "../classes/ILTIBasicLaunchData";
+
+declare const _LTI_BASIC_LAUNCH_DATA: ILTIBasicLaunchData;
+
 export const ConsentFormStateHandler: IStateHandler<STATE> =
     (session) => {
         return {
@@ -12,6 +16,9 @@ export const ConsentFormStateHandler: IStateHandler<STATE> =
                     page$("#participant-info-sheet").on("click", () => {
                         window.open("/pdf/ENGG1200%20MOOCchat%20Participant%20Information%20Sheet.pdf");
                     });
+
+                    // Fill in the name
+                    page$("#name").text(_LTI_BASIC_LAUNCH_DATA.lis_person_name_full);
 
                     // NOTE:    Both decline and accept are in regards to participation in the research study only.
                     //          ** MOOCchat will launch either way. **
