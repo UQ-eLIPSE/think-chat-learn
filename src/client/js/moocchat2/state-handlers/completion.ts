56f1a9c3a0457cc91847a115cc191bb5d4fcedd1
James Li <james.li1@uq.edu.au>
2016-08-29 12:12:51 +1000

    Fixed undefined reference when initial/revised answer not present

--- src/client/js/moocchat2/state-handlers/completion.ts
+++ src/client/js/moocchat2/state-handlers/completion.ts
@@ -40,10 +40,10 @@ export const CompletionStateHandler: IStateHandler<STATE> =
                         const initialAnswer = session.quiz.questionOptions.filter((option) => option._id === session.answers.initial.optionId)[0];
                         const revisedAnswer = session.quiz.questionOptions.filter((option) => option._id === session.answers.revised.optionId)[0];
 
-                        page$("#initial-answer-content").html(initialAnswer.content);
+                        page$("#initial-answer-content").html(initialAnswer ? initialAnswer.content : "[NO OPTION SELECTED]");
                         page$("#initial-answer-justification").text(session.answers.initial.justification);
 
-                        page$("#revised-answer-content").html(revisedAnswer.content);
+                        page$("#revised-answer-content").html(revisedAnswer ? revisedAnswer.content : "[NO OPTION SELECTED]");
                         page$("#revised-answer-justification").text(session.answers.revised.justification);
 
                         // if (virtServerData) {
