402b742be16ff26187e6f13640f66333d5d748d4
James Li <james.li1@uq.edu.au>
2017-03-21 12:09:37 +1000

    Initial answer section time now 10 minutes

--- common/config/Conf.ts
+++ common/config/Conf.ts
@@ -12,7 +12,7 @@ export const Conf = {
     },
 
     timings: {
-        initialAnswerMs: Utils.DateTime.secToMs(15 * 60),
+        initialAnswerMs: Utils.DateTime.secToMs(10 * 60),
         discussionMs: Utils.DateTime.secToMs(15 * 60),
         revisedAnswerMs: Utils.DateTime.secToMs(6 * 60) 
     },
