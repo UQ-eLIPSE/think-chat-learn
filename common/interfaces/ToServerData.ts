5ee87d82fd5c0618e2c0ddebd47506ef978e8705
Elliot Ritchie <elliot86@gmail.com>
2019-01-07 09:55:00 +1000

    Linting fixes

--- common/interfaces/ToServerData.ts
+++ common/interfaces/ToServerData.ts
@@ -7,6 +7,6 @@ export type QuestionOption = DBSchema.QuestionOption<OID>;
 export type QuestionResponse = DBSchema.QuestionResponse<OID, Date>;
 
 export type SurveyResponse = DBSchema.SurveyResponse<OID, Date>;
-export type SurveyResponse_Content = DBSchema.SurveyResponse_Content;
+export type SurveyResponse_Content = DBSchema.SurveyResponseContent;
 
 export type Mark = DBSchema.Mark<OID, Date>;
