555f5cb753b373ea13cb312f491942409d548d67
Anant Tuli <a.tuli@uq.edu.au>
2019-11-08 09:55:44 +1000

    Making config backups only if they exist, fixed permission issues on server, updated gitignore

--- server/deploy.sh
+++ server/deploy.sh
@@ -39,14 +39,35 @@ INTERMEDIATE_FOLDER=intermediate
 # Temporary local deploy folder name
 TEMP=__deploy
 
-########################
-
 # Current revision information
 CURRENT_COMMIT="\nBranch: $(git rev-parse --abbrev-ref HEAD) \n\
 Commit SHA: $(git rev-parse HEAD)\n\
 Deployed by: $USER \n\
 On: $(date)\n"
 
+######################################
+# BACK UP ORIGINAL LOCAL CONFIGURATION
+######################################
+
+# Copy local config before building (if exists)
+[ -f "config/Conf.ts" ] && cp config/Conf.ts config/temp.Conf.ts
+[ -f "../common/config/Conf.ts" ] && cp ../common/config/Conf.ts ../common/config/temp.Conf.ts
+[ -f "../client/config/Conf.ts" ] && cp ../client/config/Conf.ts ../client/config/temp.Conf.ts
+[ -f "../client/.env.$MODE" ] && cp "../client/.env.$MODE" "../client/temp.env.$MODE"
+[ -f "../admin/.env.$MODE" ] && cp "../admin/.env.$MODE" "../admin/temp.env.$MODE"
+[ -f "../intermediate/.env.$MODE" ] && cp "../intermediate/.env.$MODE" "../intermediate/temp.env.$MODE"
+
+# Set environment files based on deployment address
+cp "config/production/$SERVER" config/Conf.ts
+cp "../common/config/production/$SERVER" ../common/config/Conf.ts
+cp "../client/config/production/$SERVER" ../client/config/Conf.ts
+cp "../client/envs/$SERVER" "../client/.env.$MODE"
+cp "../admin/envs/$SERVER" "../admin/.env.$MODE"
+cp "../intermediate/envs/$SERVER" "../intermediate/.env.$MODE"
+
+#######
+# BUILD
+#######
 echo -e "Building and deploying MOOCchat using node $NODE_VERSION"
 
 # Remove previously built files
@@ -54,27 +75,6 @@ rm -rf ./$TEMP
 
 # Create local directory for deployment
 mkdir "$TEMP" "$TEMP/$STATIC_FOLDER" "$TEMP/$ADMIN_FOLDER" "$TEMP/$INTERMEDIATE_FOLDER"
-
-# Copy config before building
-cp config/Conf.ts config/temp.Conf.ts
-cp "config/production/$SERVER" config/Conf.ts
-
-cp ../common/config/Conf.ts ../common/config/temp.Conf.ts
-cp "../common/config/production/$SERVER" ../common/config/Conf.ts
-
-cp ../client/config/Conf.ts ../client/config/temp.Conf.ts
-cp "../client/config/production/$SERVER" ../client/config/Conf.ts
-
-# Set environment files based on deployment address
-cp "../client/.env.$MODE" "../client/temp.env.$MODE"
-cp "../client/envs/$SERVER" "../client/.env.$MODE"
-
-cp "../admin/.env.$MODE" "../admin/temp.env.$MODE"
-cp "../admin/envs/$SERVER" "../admin/.env.$MODE"
-
-
-cp "../intermediate/.env.$MODE" "../intermediate/temp.env.$MODE"
-cp "../intermediate/envs/$SERVER" "../intermediate/.env.$MODE"
 
 # Server
 echo -e "Building server"
@@ -101,21 +101,11 @@ echo -e "Building backup queue intermediate SPA"
 npm run build_intermediate
 cp -r ../intermediate/dist/* "$TEMP/$INTERMEDIATE_FOLDER"
 
-echo -e "Logging deployment information"
-echo -e "$CURRENT_COMMIT" > $TEMP/README.txt
+######################################
+# RESTORE ORIGINAL LOCAL CONFIGURATION
+# (since build process is complete)
+######################################
 
-# Remove existing directories on the server
-echo -e "Removing existing directories"
-
-ssh $USER@$SERVER "mkdir -p $APP_ROOT && rm -rfv $APP_ROOT/*"
-
-echo -e "Copying built files to server"
-
-# Copy built files to server
-scp -r $TEMP/* $USER@$SERVER:$APP_ROOT/
-
-
-# Restore all original local config
 echo -e "\nRestore all original local config ...\n"
 
 mv config/temp.Conf.ts config/Conf.ts
@@ -127,8 +117,26 @@ mv "../admin/temp.env.$MODE" "../admin/.env.$MODE"
 mv "../intermediate/temp.env.$MODE" "../intermediate/.env.$MODE"
 
 
-# Unfortunately due to a dependency of the common folder
-# We need to put in the name of the folder
+echo -e "Logging deployment information"
+echo -e "$CURRENT_COMMIT" > $TEMP/README.txt
+
+########################
+# REMOTE COMMANDS
+########################
+
+# Remove existing directories on the server
+echo -e "Removing existing directories on $SERVER"
+
+REMOTE_BACKUP_FOLDER="$APP_ROOT-BACKUP-$(date +%s)"
+echo -e "\nBacking up in folder $REMOTE_BACKUP_FOLDER ..."
+echo -e "\nCreating new folder $APP_ROOT"
+ssh -t $USER@$SERVER "[ -d $APP_ROOT ] && sudo cp -R $APP_ROOT $REMOTE_BACKUP_FOLDER && sudo rm -rfv $APP_ROOT && sudo mkdir -m775 $APP_ROOT && sudo chgrp sysadmin $APP_ROOT"
+
+echo -e "Copying built files to server"
+
+# Copy built files to server
+scp -r $TEMP/* $USER@$SERVER:$APP_ROOT/
+
 echo -e "Installing server dependencies"
 
-ssh $USER@$SERVER "cd $APP_ROOT/server && npm install --production && sudo chmod 775 -R $APP_ROOT"
+ssh $USER@$SERVER -t "cd $APP_ROOT/server && npm install --production"
