46c932e50eb474c2dc5d521db8c09e6c6772e3b9
EAIT\uqhla <h.la@uq.edu.au>
2020-09-18 11:48:08 +1000

    Create pagination component

---
+++ admin/src/components/Pagination.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pagination">
+    <div class="controls">
+      
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from "vue-property-decorator";
+  export class Pagination extends Vue {
+    @Prop ({required: true, default: 1}) private currentPage!: number;
+    @Prop ({required: true, default: 1}) private totalPages!: number;
+    @Prop ({required: true, default: 0}) private numPageButtons!: number;
+    @Prop ({required: true, default: () => {}}) private changePageAction!: Function;
+  }
+</script>
