442d8167993d2e8c5e1d807a222e9a70e311d856
EAIT\uqhla <h.la@uq.edu.au>
2020-09-23 16:39:40 +1000

    Revert landing page

--- admin/src/components/Landing.vue
+++ admin/src/components/Landing.vue
@@ -2,7 +2,6 @@
   <div class="container">
     <h2>Admin panel</h2>
     <span class="intro">Welcome to Think.Chat.Learn administration panel. You can mark quiz questions, setup/view quiz sessions and create/view questions</span>
-    <AutoComplete :itemList="generatedList" :value="currentValue" @click="onClick"/>
   </div>
 </template>
 
@@ -14,35 +13,6 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import AutoComplete from "../elements/AutoComplete.vue";
-
-@Component({
-  components: {
-    AutoComplete
-  }
-})
-export default class Landing extends Vue {
-  private currentValue = "value from parent";
-  get generatedList() {
-    return [
-      {
-        label: "Item1",
-        value: "item-1"
-      },
-      {
-        label: "Item2",
-        value: "item-2"
-      },
-      {
-        label: "Item3",
-        value: "item-3"
-      }
-    ]
-  }
-
-  onClick(index: number){
-    const list = this.generatedList;
-    this.currentValue = "value from parent";
-  }
-}
+@Component({})
+export default class Landing extends Vue {}
 </script>
--- admin/src/elements/AutoComplete.vue
+++ admin/src/elements/AutoComplete.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="form-control input-autocomplete">
-    <div class="editable-field">
-      <input type="text" @input="(e) => onQuery(e.currentTarget.value)" v-model="displayValue">
-      <div class="dropdown-list card-container">
+    <div :class="`${type === 'text'? 'editable-field': 'search-field'}`">
+      <input :type="type" v-model="displayValue"
+              @input="(e) => onQuery(e.currentTarget.value)"
+              @keydown.up="onArrowUp"
+              @keydown.down="onArrowDown"
+              @keydown.enter="onChooseItem(highlightItemIndex)">
 
+      <div class="dropdown-list card-container" v-if="isQuerying">
         <template v-for="(item, idx) in itemList">
-          <div :key="`item-${idx}`" class="dropdown-list-item" @click="onClickItem(idx)">
+          <div :key="`item-${idx}`" :class="`dropdown-list-item`" 
+                @click="onChooseItem(idx)">
             {{item.label}}
           </div>
         </template>
-
       </div>
     </div>
   </div>
@@ -22,16 +26,16 @@
   border-radius: 0 0 5px 5px;
   position: absolute;
   top: 30px;
+  max-height: 225px;
+
+  overflow-y: auto;
 }
 
 .dropdown-list-item{
   padding: 0.5rem 1rem;
+  height: 40px;
 
   &:hover {
-    background-color: rgba($uq, 0.1);
-  }
-
-  &.key-selected {
     background-color: rgba($uq, 0.15);
     color: $uq;
   }
@@ -46,28 +50,44 @@ export interface IDropdownItem {
   value: string;
 }
 
-@Component
 /**
  * <AutoComplete :itemList="generatedList" :value="currentValue" @click="onClick"/>
 */
+@Component
 export default class Collapsible extends Vue {
   @Prop({required: false, default: ""}) private title!: string;
   @Prop({required: false, default: ""}) private value!: string;
+  @Prop({required: true, default: "text"}) private type!: "text" | "search";
   @Prop({required: true, default: () => []}) private itemList!: IDropdownItem[];
 
-  private displayValue = this.value || "";
-  private isQuerying
+  private displayValue: string = this.value || "";
+  private isQuerying: boolean = false;
+
+  private mounted(){
+    document.addEventListener('click', this.onFocusOut);
+  }
+
+  private destroyed(){
+    document.removeEventListener('click', this.onFocusOut);
+  }
+
   onQuery(query: string){
+    if (!query || !query.length){
+      this.isQuerying = false;
+      return;
+    }
+    this.isQuerying = true;
     this.$emit('input', query);
   }
 
-  onClickItem(index: number){
+  onChooseItem(index: number){
     this.displayValue = this.itemList[index].label;
     this.$emit('click', index);
+    this.isQuerying = false;
   }
 
-  onUseArrow(){
-
+  onFocusOut(e: any){
+    if(!this.$el.contains(e.currentTarget)) this.isQuerying = false;
   }
 
 }
