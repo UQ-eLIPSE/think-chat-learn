cc388f1bb3f6330f11dbdcf9d29eb62440795ab9
Anzah Khan <anzah.khan@uq.edu.au>
2021-09-20 16:09:02 +1000

    Change Criteria

--- server/config/DefaultCriteria.ts
+++ server/config/DefaultCriteria.ts
@@ -1,30 +1,39 @@
 /**
  * Default criteria available in the system by default
  */
-const DefaultCriteria = [{
-    name: "evaluating",
-    description: "How well one evaluates"
-}, {
-    name: "interpreting",
-    description: "How well one interprets"
-}, {
-    name: "analysing",
-    description: "How well one analyses"
-}, {
-    name: "makingArguments",
-    description: "How well one makes arguments"
-}, {
-    name: "accuracyOfArguments",
-    description: "How accurate are one's arguments"
-}, {
-    name: "expressingAndResponding",
-    description: "How well one expresses and responds to the prompt"
-}, {
-    name: "depthOfReflection",
-    description: "How well one reflects on learning outcomes"
-}, {
-    name: "mark",
-    description: "Your mark"
-}];
+const DefaultCriteria = [
+	{
+		name: "Evaluating",
+		description: " ",
+	},
+	{
+		name: "Interpreting",
+		description: " ",
+	},
+	{
+		name: "Analysing",
+		description: " ",
+	},
+	{
+		name: "Making Arguments",
+		description: " ",
+	},
+	{
+		name: "Accuracy Of Arguments",
+		description: " ",
+	},
+	{
+		name: "Expressing And Responding",
+		description: " ",
+	},
+	{
+		name: "Depth Of Reflection",
+		description: " ",
+	},
+	{
+		name: "Mark",
+		description: " ",
+	},
+];
 
 export default DefaultCriteria;
